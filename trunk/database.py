72ae94ffdef88acda943407a5f2690fa9e1ca712
latifalam <latifalam@89b950ea-4958-11de-acc5-7fb027fc3da9>
2009-12-16 15:53:54 +0000

    Autoversioning commit:  a non-deltaV client made a change to /trunk/database.py

--- trunk/database.py
+++ trunk/database.py
@@ -9,7 +9,7 @@ DB_USER = 'python'
 DB_PASSWD = 'rock+bait'
 DB_HOST = '127.0.0.1'
 DB_PORT = 3306
-DB_NAME = 'audiowikibeta'
+DB_NAME = 'audiowikiIndia'
 
 class Database:
     def __init__(self,db_port=DB_PORT,db_host=DB_HOST,
@@ -19,13 +19,6 @@ class Database:
         self.c = self.db.cursor()
         self.c.execute('USE '+db_name+';')
 
-    def getLanguages(self):
-        self.c.execute(
-            "SELECT language, audioFile, digit FROM languages;")
-        results = self.c.fetchall()
-        languages = [i[0] for i in results]
-        return languages;
-
     def addUser(self, phoneNumberString):
         self.c.execute("INSERT INTO users (phone_number) " + \
                        "VALUES (%s);",(str(phoneNumberString),))
@@ -33,60 +26,21 @@ class Database:
 
     def isUser(self, phoneNumberString):
         count = self.c.execute(
-            "SELECT phone_number FROM users WHERE phone_number = %s;",(str(phoneNumberString),))
+            "SELECT phone_number FROM users WHERE phone_number = %s;"
+                                            ,(str(phoneNumberString),))
         return count>0
 
-    def getKeys(self):
-        self.c.execute("""SELECT `key` FROM categories ORDER BY `key` ASC;""")
-        results = self.c.fetchall()
-        keys = [i[0] for i in results]
-        return keys
-   
-    def getCommentList(self, phoneNumberString, categoryKey, language):        
-        self.c.execute("SELECT id from comments WHERE `key` = %s AND " + \
-                          "language = %s ORDER BY time DESC;", \
-                       (str(categoryKey), language.lower(),))
-        results = self.c.fetchall()
-        commentList = [i[0] for i in results]
-        debugPrint(str(commentList))
-        return commentList
-
-    def getAllComments(self, categoryKey, language):
-        debugPrint(str(language))
-        self.c.execute("""SELECT id FROM comments WHERE `key` = %s and
-                                language = %s ORDER BY time DESC;""", \
-                        (str(categoryKey), language.lower(),))
-        results = self.c.fetchall()
-        allComments = [i[0] for i in results]
-        return allComments
-
-    def getCursor(self, phoneNumberString):
-        count = self.c.execute(
-            "SELECT thread_cursor FROM users WHERE phone_number = %s;",
-            (phoneNumberString,))
-        assert(count == 1L) # If the assertion is true, the program continues
-                            # Otherwise, the program stops and brings up an
-                            # AssertionError with this line.
-        cursor = _parseCursor(self.c.fetchall()[0][0])
-        return cursor
-
-    def updateUserCursor(self, phoneNumberString, categoryKey, commentID):
-        userLocation = self.getCursor(phoneNumberString)
-        userLocation[str(categoryKey)] = str(commentID)
-        formattedString = _formatCursor(userLocation)
-        self.c.execute(
-            "UPDATE users SET thread_cursor = %s WHERE phone_number = %s;",
-            (formattedString, phoneNumberString))
-        self.db.commit()
-
-    def hasPlayed(self, commentID):
-        self.c.execute("""UPDATE comments SET skip_count = skip_count - 1
-                            WHERE id = %s;""", (int(commentID),))
-        debugPrint(str(commentID) + " HAS FINISHED PLAYING")
-
-    def addComment(self,categoryKey,phoneNum,language):
-        self.c.execute("INSERT INTO comments (`key`, commenter, language) VALUES (%s,%s, %s);", \
-                       (categoryKey,phoneNum,language))
+    def getCommentIDs(self):
+        self.c.execute("""SELECT id from comments WHERE archived = 0 \
+                        ORDER BY time DESC;""")
+        # Select the comments that haven't been archived.
+        comments = self.c.fetchall()
+        comments = [i[0] for i in comments]
+        return comments
+           
+    def addComment(self,phoneNum):
+        self.c.execute("INSERT INTO comments (user) VALUES (%s);", \
+                       (phoneNum))
         self.db.commit()
         return self.c.lastrowid
 
@@ -95,54 +49,4 @@ class Database:
             "UPDATE comments SET skip_count = skip_count + 1 WHERE id = %s;",
             (commentID))
         self.db.commit()
-        self.c.execute(
-            "SELECT `key` FROM comments WHERE id = '%s';",
-            (commentID))
-        results = self.c.fetchall()
-        key = [i[0] for i in results]
-        debugPrint("SKIPPED " + str(commentID) + ", IN CATEGORY, " + str(key))
-
-    def getPrivateForumIntroFileName(self, forumKey):
-        self.c.execute(
-            "SELECT introFileName FROM privateForums WHERE forumKey = %s;",
-            (forumKey))
-        self.db.commit()
-        results = self.c.fetchall()
-        if results:
-            debugPrint(str(results))
-            introFileName = [i[0] for i in results][0]
-            return introFileName
-        else:
-            return ""
-
-def _parseCursor(string):
-    """
-    Parses a string in the format ((\w+):(\w+),)* into a dict where the
-    key is to the left of the ':' and the value is between the ':' and the ','
-
-    Note that if string is not a string, this returns an empty dictionary.
-    
-    Inverse function is formatThreadCursor
-    """
-    if (not string) or (not isinstance(string,str)):
-        return {}
-    groups = re.findall('(\w+):(\w+),',string)
-    return dict(groups)
-
-def _formatCursor(parsedDict):
-    """
-    Converts a dictionary into the string format:
-    [key]:[value],
-    appended to one another.
-
-    Note that key and value must be strictly alphanumeric strings;
-    raises a value error if this condition isn't met.
-    
-    Inverse function is parseThreadCursor.
-    """
-    res = ''
-    for key in parsedDict:
-        if (not key.isalnum() or not parsedDict[key].isalnum):
-            raise ValueError('Key or value contained non alphanumerics')
-        res += key + ':' + parsedDict[key] + ','
-    return res
+        debugPrint("SKIPPED "+str(commentID))
