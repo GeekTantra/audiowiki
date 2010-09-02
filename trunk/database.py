970f41b5b196e3d5cb0b6259dbaaf91f591d0ed6
samujjalp <samujjalp@89b950ea-4958-11de-acc5-7fb027fc3da9>
2010-09-02 18:58:17 +0000

    Major update: Updated all modified config files and AGI Scripts & added new files for cdr, analytics, etc.

--- trunk/database.py
+++ trunk/database.py
@@ -5,11 +5,11 @@ import MySQLdb
 import re
 from utilities import *
 
-DB_USER = 'python'
-DB_PASSWD = 'rock+bait'
+DB_USER = 'spark'
+DB_PASSWD = 'Ath3n@1094'
 DB_HOST = '127.0.0.1'
 DB_PORT = 3306
-DB_NAME = 'audiowikiIndia'
+DB_NAME = 'audiwikiswara'
 
 class Database:
     def __init__(self,db_port=DB_PORT,db_host=DB_HOST,
@@ -25,19 +25,19 @@ class Database:
         return count>0
         
     def getPostsInChannel(self, channelNum):
-        self.c.execute("SELECT * FROM comments WHERE station = %s and archived = 0 ORDER BY time DESC;",
+        self.c.execute("SELECT * FROM lb_postings WHERE station = %s and status = 3 ORDER BY posted DESC;",
 						(str(channelNum),))
         posts = self.c.fetchall()
         posts = [i[0] for i in posts]
         return posts
 
     def publishPost(self, postID):
-        self.c.execute("UPDATE comments SET archived = 0 WHERE id = %s;",
+        self.c.execute("UPDATE lb_postings SET status = 3 WHERE id = %s;",
 						(str(postID),))
         self.db.commit()
 
     def archivePost(self, postID):
-        self.c.execute("UPDATE comments SET archived = 1 WHERE id = %s;",
+        self.c.execute("UPDATE lb_postings SET status = 2 WHERE id = %s;",
 						(str(postID),))
 	self.db.commit()
 
@@ -57,35 +57,63 @@ class Database:
         return count>0
 
     def getCommentIDs(self):
-        self.c.execute("""SELECT id from comments WHERE archived = 0 \
-                        ORDER BY time DESC;""")
+        self.c.execute("""SELECT id from lb_postings WHERE status = 3 \
+                        ORDER BY posted DESC;""")
         # Select the comments that haven't been archived.
         comments = self.c.fetchall()
         comments = [i[0] for i in comments]
         return comments
 
     def getAllCommentIDs(self):
-        self.c.execute("""SELECT id from comments ORDER BY time DESC;""")
+        self.c.execute("""SELECT id from lb_postings ORDER BY posted DESC;""")
         # Select the comments that haven't been archived.
         comments = self.c.fetchall()
         comments = [i[0] for i in comments]
         return comments
 
+   
     def addCommentToChannel(self, phoneNum, channel):
-        self.c.execute("INSERT INTO comments (user, station) VALUES (%s, %s);", \
-                       (phoneNum, str(channel),))
-        self.db.commit()
-        return self.c.lastrowid
-
-    def addComment(self,phoneNum):
-        self.c.execute("INSERT INTO comments (user) VALUES (%s);", \
+		self.c.execute("INSERT INTO lb_postings (user, station) VALUES (%s, %s);",(phoneNum, str(channel),))
+		self.db.commit()
+		ids = str(self.c.lastrowid)
+		extension = '.mp3'	
+		filename = ids + extension
+		print filename
+		self.c.execute("UPDATE lb_postings SET audio_file = %s WHERE id = %s;",(filename, ids)) 
+		self.db.commit()
+		return ids
+		
+    def addComment(self, phoneNum):
+        self.c.execute("INSERT INTO lb_postings (user) VALUES (%s);", \
                        (phoneNum))
         self.db.commit()
         return self.c.lastrowid
 
     def skipComment(self, commentID):
         self.c.execute(
-            "UPDATE comments SET skip_count = skip_count + 1 WHERE id = %s;",
+            "UPDATE lb_postings SET skip_count = skip_count + 1 WHERE id = %s;",
             (commentID))
         self.db.commit()
         debugPrint("SKIPPED "+str(commentID))
+		
+    def addPlaybackEvent(self, postID, duration):
+        self.c.execute("INSERT INTO analytics (eventype, msglstnd, durlistndto) VALUES (%s, %s, %s);",('Listened', str(postID), str(duration),))
+	self.db.commit()
+	return self.c.lastrowid
+		
+    def addSkipEvent(self, postID, duration):
+        self.c.execute("INSERT INTO analytics (eventype, msglstnd, durlistndto) VALUES (%s, %s, %s);",('Skipped', str(postID), str(duration),))
+	self.db.commit()
+		
+    def addInvalidkeyEvent(self, key, when, duration):
+        self.c.execute("INSERT INTO analytics (eventype, invdgtpsd, context, whenpressed) VALUES (%s, %s, %s, %s);",('Invalid Keypress', str(key), str(when), str(duration),))
+        self.db.commit()
+	
+    def addMessageRecordEvent(self, postID):
+        self.c.execute("INSERT INTO analytics (eventype, msgrcd) VALUES (%s, %s);",('Recorded', str(postID),))
+	self.db.commit()
+	
+    
+    def getID(self):
+        self.c.execute("""SELECT id FROM cdr ORDER BY calldate DESC LIMIT 1;""")
+        callidno = self.c.fetchall()
