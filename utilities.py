77bfbb03edbec47aca626e0cab420d7bbe1234c1
geektantra <geektantra@89b950ea-4958-11de-acc5-7fb027fc3da9>
2011-02-27 06:52:14 +0000

    Moved newsMenu to next menu level and added entertainment news to the newsMenu.

--- utilities.py
+++ utilities.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+import random
 
 def debugPrint(str):
     sys.stderr.write(str+'\n')
@@ -28,3 +29,35 @@ def Nop():
 
 def removeTempFile(fname):
     os.remove(fname)
+
+def coloredText(string, color=False, bold=False):
+    colors_dict = {
+        'gray': '30',
+        'red': '31',
+        'green': '32',
+        'yellow': '33',
+        'blue': '34',
+        'magenta': '35',
+        'cyan': '36',
+        'white': '37',
+        'crimson': '38',
+        'highlighted_red': '41',
+        'highlighted_green': '42',
+        'highlighted_brown': '43',
+        'highlighted_blue': '44',
+        'highlighted_magenta': '45',
+        'highlighted_cyan': '46',
+        'highlighted_gray': '47',
+        'highlighted_crimson': '48',
+    }
+    if colors_dict.has_key(color):
+        bold_attr = ('', '1')[bold]
+        return '\033['+ bold_attr +';'+colors_dict[color] + 'm' + string + '\033['+ bold_attr +';m'
+    else:
+        return string
+
+def keyPressCLIPrompt(string, key):
+    key_colors = ['highlighted_red', 'highlighted_green', 'highlighted_brown', 'highlighted_blue', 'highlighted_magenta', 'highlighted_cyan', 'highlighted_gray', 'highlighted_crimson', ]
+    random.shuffle(key_colors)
+    return coloredText("\t--"+'Press ', 'blue') + coloredText(key, key_colors[0], True) + coloredText(' for ', 'blue') + coloredText(string, 'blue', True) + coloredText('.', 'blue')
+
