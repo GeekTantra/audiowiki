383438fc422f5c8c1ca88e2166e983372582622f
arjunsdarkside@gmail.com <arjunsdarkside@gmail.com@89b950ea-4958-11de-acc5-7fb027fc3da9>
2011-05-04 08:01:11 +0000

    Youtube integration complete

--- branches/arjun/youtubeswarastory.py
+++ branches/arjun/youtubeswarastory.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+#Requires gdata and elementtree
 # Requires tweepy,simplejson and oauth0
 # further requires a config file (tweetrswara.conf) with the access token for the swara user
 
@@ -15,7 +15,7 @@ import gdata.youtube
 import gdata.youtube.service
 
 config=ConfigParser.ConfigParser()
-config.read("./twittercredsarjunven.conf")
+config.read("/home/swara/audiowiki/twittercredsswara.conf")
 
 # Create a client class which will make HTTP requests with Google Docs server.
 yt_service = gdata.youtube.service.YouTubeService()
@@ -28,18 +28,6 @@ yt_service.password = 'kissmyass3ce'
 yt_service.ProgrammaticLogin()
 
 
-# prepare a media group object to hold our video's meta-data
-my_media_group = gdata.media.Group(
-  title=gdata.media.Title(text='My Test Movie'),
-  description=gdata.media.Description(description_type='plain',
-                                      text='My description'),
-  keywords=gdata.media.Keywords(text='cars, funny'),
-  category=[gdata.media.Category(
-      text='Autos',
-      scheme='http://gdata.youtube.com/schemas/2007/categories.cat',
-      label='Autos')],
-  player=None
-)
 
 
 
@@ -62,10 +50,7 @@ def getLastPushedPostID():
 	post=os.popen("cat lastpushedpost").read().strip()
 	return post
 
-def youtubetweet(post,url):
-	title=db.getTitleforPost(12345,post)[0]
-	if len(title) > 120:
-		title=title[:120]
+def youtubetweet(post,url, title):
 	#url="http://cgnetswara.org/index.php?id="+str(post)
 	print title + " " + url
 	status=title +" "+ url
@@ -76,24 +61,48 @@ if __name__=="__main__":
 	list  = os.popen("ls -A /home/swara/audiowiki/web/videos/*.avi").read().strip('').split('\n')
 	#print list
 	for post in list:
-		if post == '':
+		postid = post.split('/')[6].split('.')[0]
+		if postid == '':
 			break
+		# prepare a geo.where object to hold the geographical location
+		# of where the video was recorded
+		where = gdata.geo.Where()
+		where.set_location((37.0,-122.0))
+		postid = post.split('/')[6].split('.')[0]
+		title=db.getTitleforPost(12345,postid)[0]
+		if len(title) > 120:
+			title=title[:120]
+		# create the gdata.youtube.YouTubeVideoEntry to be uploaded
+		
+		# prepare a media group object to hold our video's meta-data
+		my_media_group = gdata.media.Group(
+		  title=gdata.media.Title(text=title),
+		  description=gdata.media.Description(description_type='plain',
+						      text=postid),
+		  keywords=gdata.media.Keywords(text='cgnet,swara,news,community,mobile'),
+		  category=[gdata.media.Category(
+		      text='News',
+		      scheme='http://gdata.youtube.com/schemas/2007/categories.cat',
+		      label='news')],
+		  player=None
+		)
 		# prepare a geo.where object to hold the geographical location
 		# of where the video was recorded
 		where = gdata.geo.Where()
 		where.set_location((37.0,-122.0))
 
 		# create the gdata.youtube.YouTubeVideoEntry to be uploaded
-		video_entry = gdata.youtube.YouTubeVideoEntry(media=my_media_group,geo=where)
+		video_entry = gdata.youtube.YouTubeVideoEntry(media=my_media_group,
+                                              geo=where)
 
 		# set the path for the video file binary
 		video_file_location = post
-		postid = post.split('/')[6].split('.')[0]
 		new_entry = yt_service.InsertVideoEntry(video_entry, video_file_location)
 
 		youtubeurl = new_entry.media.player.url.split('&')[0]
 		print postid + " " + youtubeurl
-		youtubetweet(postid,youtubeurl)
+		youtubetweet(postid,youtubeurl,title)
+		os.system("rm -rf %s" %post)
 	#print "Final post = " + str(post)
 	#os.system("echo %s > lastpushedpost" %(str(post)))
 	
