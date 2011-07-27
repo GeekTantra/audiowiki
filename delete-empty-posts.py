993b40c05fa20cfc3654e16268da3ca83b3bea07
arjunsdarkside@gmail.com <arjunsdarkside@gmail.com@89b950ea-4958-11de-acc5-7fb027fc3da9>
2011-07-27 12:27:05 +0000

    added junk post deletion

--- delete-empty-posts.py
+++ delete-empty-posts.py
@@ -4,14 +4,34 @@
 import sys,os
 from utilities import *
 from database import *
+from mutagen.mp3 import MP3
+import time;
 if __name__=="__main__":
-	db = Database()
-	list=os.popen("for i in `ls /home/swara/audiowiki/web/sounds/web/*.mp3`;do echo $i `mplayer -vo null -ao null -frames 0 -identify $i  2>/dev/null | grep ID_LENGTH | awk -F= '{print $2}'`;done").read().strip().split('\n')
-	for line in list:
-		lenpost=line.split(' ')[1]
-		if float(lenpost) < 5:
-			filename=line.split(' ')[0]
-			post = filename.split("sounds/web/")[1].split('.')[0]
-			db.deletePost(post)
-			print ("Deleted short post %s length was %s" %(post,lenpost))
-			os.system("rm %s" %filename)
+	db=Database()
+	while True:
+		timestamp=os.popen("date +%H:%M-%Y%m%d").read().strip()
+		#print "Cleaning empty files at %s \n" %timestamp
+		posts = db.getAllPostsInChannel('12345')
+		for post in posts:
+			line="/home/swara/audiowiki/web/sounds/web/%s.mp3" %str(post)	
+			try:
+				audio = MP3(line)
+			except IOError:
+				print "MP3 file not found for post %s deleting post" %line
+				db.deletePost(post)
+				pass
+			#print line,audio.info.length
+			if float(audio.info.length) < 20:
+				filename=line
+				#post = filename.split("sounds/web/")[0].split('.')[0]
+				db.deletePost(post)
+				with open("/var/log/swara.log", "a") as f:
+					f.write("Deleted short post %s length was %s \n" %(post,audio.info.length))
+				#print "Deleted short post %s length was %s \n" %(post,audio.info.length)
+				#os.system("rm %s" %filename)
+				os.system("mv %s /home/swara/audiowiki/web/sounds/web/trash" %filename)
+		timestamp=os.popen("date +%H:%M-%Y%m%d").read().strip()
+		with open("/var/log/swara.log", "a") as f:
+			f.write("Cleaned empty files at %s \n" %timestamp)
+		#print "Cleaned empty files at %s \n" %timestamp
+		time.sleep(180)
